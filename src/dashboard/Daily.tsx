38ca6fefbf58b3bf120022f51b57d0de15e97811
Ramón Miklus <ramon.miklus@gmail.com>
2020-04-13 21:28:02 +0200

    bar chart cursor

--- src/dashboard/Daily.tsx
+++ src/dashboard/Daily.tsx
@@ -43,7 +43,7 @@ const Daily = ({
           <CartesianGrid stroke="#636363" strokeDasharray="3 3" />
           <XAxis dataKey="name" stroke="#eee"/>
           <YAxis stroke="#eee" />
-          <Tooltip contentStyle={{backgroundColor: "#282c34"}}/>
+          <Tooltip cursor={false} contentStyle={{backgroundColor: "#282c34"}}/>
           <Bar dataKey="totale_positivi" name={localisation.positives} barSize={60} fill={COLORS[0]} />
           <Bar dataKey="dimessi_guariti" name={localisation.recovered} barSize={60} fill={COLORS[1]} />
           <Bar dataKey="deceduti" name={localisation.deaths} barSize={60} fill={COLORS[2]} />
